bda535f215d2fea75e37470b87207b0560d09dcc
Yazan KASSAM <yazankassam.codavia@gmail.com>
2021-06-20 07:36:31 +0000

    remove last docker images of this micro service

--- deploy_instructions.sh
+++ deploy_instructions.sh
@@ -2,9 +2,15 @@
 
 DOCKER_ORGANIZATION_NAME=$1
 DOCKER_IMAGE_NAME=$2
+
 echo ======== docker images ========;
 docker images --format {{.Repository}}:{{.Tag}};
+
 echo ======== docker containers ========;
 docker ps --format {{.Names}};
+
 echo ======== last docker images of this micro service ========;
 docker images -q --filter reference=${DOCKER_ORGANIZATION_NAME}/${DOCKER_IMAGE_NAME};
+
+echo ======== remove last docker images of this micro service ========;
+docker images -q --filter reference=${DOCKER_ORGANIZATION_NAME}/${DOCKER_IMAGE_NAME} | grep -q . && docker rmi $(docker images --format="{{.Repository}} {{.ID}}" |  grep "^${DOCKER_ORGANIZATION_NAME}/${DOCKER_IMAGE_NAME} " |  cut -d" " -f2 | tr "\r\n" " ") || echo Not Found Image With Name = ${DOCKER_IMAGE_NAME};
