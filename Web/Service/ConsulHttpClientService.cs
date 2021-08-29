4681ab89ddd7ba541fa812d0284689290095c80e
yazan.codavia <yazankassam.codavia@gmail.com>
2021-08-29 12:30:54 +0300

    setup auth connection

--- Web/Service/ConsulHttpClientService.cs
+++ Web/Service/ConsulHttpClientService.cs
@@ -7,7 +7,6 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Consul;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
@@ -17,15 +16,11 @@ namespace iread_story.Web.Service
     {
         private readonly HttpClient _client;
         private IConsulClient _consulClient;
-        private IHttpContextAccessor _httpContextAccessor;
 
-        public ConsulHttpClientService(HttpClient client,
-        IHttpContextAccessor httpContextAccessor,
-         IConsulClient consulclient)
+        public ConsulHttpClientService(HttpClient client, IConsulClient consulclient)
         {
             _client = client;
             _consulClient = consulclient;
-            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<T> GetAsync<T>(string serviceName, string requestUri)
@@ -39,13 +34,7 @@ namespace iread_story.Web.Service
                 return default(T);
             }
 
-            string token = await _httpContextAccessor.HttpContext.GetTokenAsync("access_token");
-
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
             var content = await response.Content.ReadAsStringAsync();
-
-
 
             return JsonConvert.DeserializeObject<T>(content);
         }
@@ -68,7 +57,8 @@ namespace iread_story.Web.Service
 
         }
 
-        public async Task<T> PostFormAsync<T>(string serviceName, string requestUri, Dictionary<string, string> parameters, List<IFormFile>? attachments)
+        public async Task<T> PostFormAsync<T>(string serviceName, string requestUri,
+            Dictionary<string, string> parameters, List<IFormFile>? attachments)
         {
             var uri = await GetRequestUriAsync(serviceName, requestUri);
 
@@ -98,34 +88,6 @@ namespace iread_story.Web.Service
                 }
             }
             var response = await _client.PostAsync(uri, formDataContent);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                return default(T);
-            }
-
-            var content = await response.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<T>(content);
-        }
-
-
-        public async Task<T> PostFormAsync<T>(string serviceName, string requestUri, Dictionary<string, string> parameters, Object obj)
-        {
-            var uri = await GetRequestUriAsync(serviceName, requestUri);
-
-            var formDataContent = new MultipartFormDataContent();
-
-            if (parameters != null)
-            {
-                foreach (var parameter in parameters)
-                {
-                    formDataContent.Add(new StringContent(parameter.Value, Encoding.UTF8, "application/json"), parameter.Key);
-                }
-            }
-            formDataContent.Add(new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json"));
-            var response = await _client.PostAsync(uri, formDataContent);
-
 
             if (!response.IsSuccessStatusCode)
             {
@@ -196,6 +158,9 @@ namespace iread_story.Web.Service
         }
 
 
+
+
+
         private async Task<Uri> GetRequestUriAsync(string serviceName, string uri)
         {
             //Get all services registered on Consul
@@ -237,5 +202,55 @@ namespace iread_story.Web.Service
             return servToUse;
         }
 
+        public async Task<T> PostFormAsync<T>(string serviceName, string requestUri, Dictionary<string, string> parameters, Object obj)
+        {
+            var uri = await GetRequestUriAsync(serviceName, requestUri);
+
+            var formDataContent = new MultipartFormDataContent();
+
+            if (parameters != null)
+            {
+                foreach (var parameter in parameters)
+                {
+                    formDataContent.Add(new StringContent(parameter.Value, Encoding.UTF8, "application/json"), parameter.Key);
+                }
+            }
+            formDataContent.Add(new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json"));
+            var response = await _client.PostAsync(uri, formDataContent);
+
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return default(T);
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        public AgentService GetAgentService(string serviceName)
+        {
+
+            //Get all services registered on Consul
+            var allRegisteredServices = _consulClient.Agent.Services().Result;
+
+            //Get all instance of the service went to send a request to
+            var registeredServices = allRegisteredServices.Response?.Where(s => s.Value.Service.Equals(serviceName, StringComparison.OrdinalIgnoreCase)).Select(x => x.Value).ToList();
+
+            if (registeredServices.Count < 1)
+            {
+                throw new Exception($"Consul service: '{serviceName}' was not found.");
+            }
+            //Get a random instance of the service
+            var service = GetRandomInstance(registeredServices, serviceName);
+
+            if (service == null)
+            {
+                throw new Exception($"Consul service: '{serviceName}' was not found.");
+            }
+
+            return service;
+        }
     }
 }
