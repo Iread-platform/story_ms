e8357ad8014dca79b019bcbea4a1d2e5c109c4e9
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-10-13 12:44:53 +0300

    IR-245 Add language and story entities relationships.

--- Migrations/20211013094152_story_language_connection.Designer.cs
+++ Migrations/20211013094152_story_language_connection.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iread_story.DataAccess.Data;
 
 namespace iread_story.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211013094152_story_language_connection")]
+    partial class story_language_connection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
